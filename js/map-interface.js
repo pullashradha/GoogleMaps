3f922578a0b3036dcc87ce58f52bd84371d25237
Alexander Jacks and Shradha Pulla <shradha@>
2016-08-03 10:59:36 -0700

    Add bower_components link to index.html, bower reads jQuery, error with api access request

--- js/map-interface.js
+++ js/map-interface.js
@@ -2,7 +2,7 @@ var apiKey = require('./../.env').apiKey;
 var GoogleMap = require("./../js/map.js").mapModule;
 
 $(document).ready(function() {
-  $.get('https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&callback=initMap') function(response) {
-    $('.showMap').show(GoogleMap);
+  $.get('https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&callback=initMap', function() {
+    $('#showMap').show(GoogleMap);
   });
 });
--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 function initMap() {
-  var mapDiv = document.getElementById("map");
+  var mapDiv = document.getElementById("showMap");
   var map = new google.maps.Map(mapDiv, {
     center: {lat: 45.5231, lng: 122.6765},
     zoom: 8
