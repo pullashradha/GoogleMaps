8987b8b054a48df78efbb0f788ebef34dea6d5d2
Alexander Jacks and Shradha Pulla <alexander+shradha@>
2016-08-03 11:43:59 -0700

    Add CORS application for Chrome, refactor map.js, no errors in console but no map shows

--- js/map-interface.js
+++ js/map-interface.js
@@ -3,6 +3,6 @@ var GoogleMap = require("./../js/map.js").mapModule;
 
 $(document).ready(function() {
   $.get('https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&callback=initMap', function() {
-    $('#showMap').show(GoogleMap);
+    $('#showMap').show();
   });
 });
--- js/map.js
+++ js/map.js
@@ -1,9 +1,10 @@
 function initMap() {
   var mapDiv = document.getElementById("showMap");
-  var map = new google.maps.Map(mapDiv, {
+  var mapCoordinates = {
     center: {lat: 45.5231, lng: 122.6765},
     zoom: 8
-  });
+  };
+  var map = new google.maps.Map(mapDiv, mapCoordinates);
 }
 
 exports.mapModule = initMap;
