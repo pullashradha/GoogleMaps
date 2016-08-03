e97d04f5ff4eb5834ede28107f86e0c7b67d90ca
Alexander Jacks and Shradha Pulla <alexander+shradha@>
2016-08-03 15:53:58 -0700

    Working on gettin directons to another location pin on the maps

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,13 +1,92 @@
 var apiKey = require('./../.env').apiKey;
 
+//User Functions
+function geolocationSuccess(position) {
+  var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+  var myOptions = {
+    zoom : 16,
+    center : userLatLng,
+    mapTypeId : google.maps.MapTypeId.DIRECTIONS
+  };
+  var mapObject = new google.maps.Map(document.getElementById("showMap"), myOptions);
+  new google.maps.Marker({
+    map: mapObject,
+    position: userLatLng
+  });
+}
+
+function locateUser() {
+  if (navigator.geolocation){
+    var positionOptions = {
+      enableHighAccuracy: true,
+      timeout: 1000 * 1000
+    };
+    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+  }
+  else {
+    alert("Your browser doesn't support the Geolocation API");
+  }
+}
+
+//Geolocation Error Function
+function geolocationError(positionError) {
+  alert(positionError);
+}
+
+//Destination Functions
+function destinationSuccess(destination) {
+  var destinationLatLng = new google.maps.LatLng(destination.coords.latitude, destination.coords.longitude);
+  console.log(destinationLatLng);
+  var myOptions = {
+    zoom : 16,
+    center : destinationLatLng,
+    mapTypeId : google.maps.MapTypeId.ROADMAP
+  };
+  var mapObject = new google.maps.Map(document.getElementById("showMap"), myOptions);
+  new google.maps.Marker({
+    map: mapObject,
+    destination: destinationLatLng
+  });
+}
+
+function locateDestination() {
+  if (navigator.geolocation){
+    var positionOptions = {
+      enableHighAccuracy: true,
+      timeout: 1000 * 1000
+    };
+    navigator.geolocation.getCurrentPosition(destinationSuccess, geolocationError, positionOptions);
+  }
+  else {
+    alert("Your browser doesn't support the Geolocation API");
+  }
+}
+
+//Direction Functions
+// var directionsDisplay = new google.maps.DirectionsRenderer({
+//   map: map
+// });
+//
+// var request = {
+//   destination: destinationLatLng,
+//   origin: userLatLng,
+//   travelMode: 'WALKING'
+// };
+//
+// var directionsService = new google.maps.DirectionsService();
+// directionsService.route(request, function(response, status) {
+//   if (status == 'OK') {
+//     directionsDisplay.setDirections(response);
+//   }
+// });
+
 
 $(document).ready(function() {
-  $.get('https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&callback=initMap', function initMap() {
-    var mapDiv = document.getElementById("showMap");
-    var mapCoordinates = {
-      center: {lat: 45.5231, lng: -122.6765},
-      zoom: 8
-    }
-    var map = new google.maps.Map(mapDiv, mapCoordinates);
+  $("#userLocation").click(locateUser);
+  $("#userDestination-btn").click(function(){
+    var destination = $("#userDestination").val();
+    var destinationResult = locateDestination(destination);
+    // $("#printout").show(destinationResult);
   });
 });
