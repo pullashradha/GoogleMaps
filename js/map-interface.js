91edc6e1e96a10ef3c79283a7ddbf2f68c149703
Alexander Jacks and Shradha Pulla <alexander+shradha@>
2016-08-03 13:52:19 -0700

    Google maps displays on index

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,8 +1,13 @@
 var apiKey = require('./../.env').apiKey;
-var GoogleMap = require("./../js/map.js").mapModule;
+
 
 $(document).ready(function() {
-  $.get('https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&callback=initMap', function() {
-    $('#showMap').show();
+  $.get('https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&callback=initMap', function initMap() {
+    var mapDiv = document.getElementById("showMap");
+    var mapCoordinates = {
+      center: {lat: 45.5231, lng: -122.6765},
+      zoom: 8
+    }
+    var map = new google.maps.Map(mapDiv, mapCoordinates);
   });
 });
--- js/map.js
+++ js/map.js
@@ -1,10 +1,10 @@
-function initMap() {
-  var mapDiv = document.getElementById("showMap");
-  var mapCoordinates = {
-    center: {lat: 45.5231, lng: 122.6765},
-    zoom: 8
-  };
-  var map = new google.maps.Map(mapDiv, mapCoordinates);
-}
-
-exports.mapModule = initMap;
+// function initMap() {
+//   var mapDiv = document.getElementById("showMap");
+//   var mapCoordinates = {
+//     center: {lat: 45.5231, lng: 122.6765},
+//     zoom: 8
+//   };
+//   var map = new google.maps.Map(mapDiv, mapCoordinates);
+// }
+//
+// exports.mapModule = initMap;
