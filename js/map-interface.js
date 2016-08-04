a249c382984f73fc013ec0f46dbfb9b5ba18a07a
Alexander Jacks and Shradha Pulla <shradha@>
2016-08-04 13:22:44 -0700

    splits JS into interface and backend modules; full functonality of search bar

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,27 +1,4 @@
-// var apiKey = require('./../.env').apiKey;
-
-var createMap = function(cityInput) {
-  // Draw map based on default location
-  var map = new google.maps.Map(document.getElementById("showMap"), {
-    center: {lat: -34.397, lng: 150.644}, //Default location
-    scrollwheel: false,
-    zoom: 12
-  });
-  // Update center of map based on inputted city name
-  var geocoder = new google.maps.Geocoder();
-  geocoder.geocode( {"address": cityInput}, function(results, status) {
-    if (status === "OK") {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: map,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert("Geocode was not successful for the following reason: " + status);
-    }
-  });
-}
-
+var createMap = require("./../js/map.js").mapModule;
 
 $(document).ready(function(event) {
   $("#city-form").submit(function(event) {
--- js/map.js
+++ js/map.js
@@ -1,1 +1,25 @@
+// var apiKey = require('./../.env').apiKey;
 
+var createMap = function(cityInput) {
+  // Draw map based on default location
+  var map = new google.maps.Map(document.getElementById("showMap"), {
+    center: {lat: -34.397, lng: 150.644}, //Default location
+    scrollwheel: false,
+    zoom: 16
+  });
+  // Update center of map based on inputted city name
+  var geocoder = new google.maps.Geocoder();
+  geocoder.geocode( {"address": cityInput}, function(results, status) {
+    if (status === "OK") {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+
+exports.mapModule = createMap;
