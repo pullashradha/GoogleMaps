4c05fa3129112b65b5469203df9b08d984ada2ec
Alexander Jacks and Shradha Pulla <alexander+shradha@>
2016-08-04 12:13:31 -0700

    Create new custom function to create map, map works based on user inputting a city name, add slight styling to page

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,53 +1,32 @@
 // var apiKey = require('./../.env').apiKey;
-$(document).ready(function() {
-  // debugger;
+
+var createMap = function(cityInput) {
+  // Draw map based on default location
+  var map = new google.maps.Map(document.getElementById("showMap"), {
+    center: {lat: -34.397, lng: 150.644}, //Default location
+    scrollwheel: false,
+    zoom: 12
+  });
+  // Update center of map based on inputted city name
   var geocoder = new google.maps.Geocoder();
-  function geocodeAddress(geocoder, resultsMap) {
-    var input1 = $("#input1").val();
-    // console.log(input1);
-    geocoder.geocode({"#input1" : input1}, function(results, status) {
-      if (status === 'OK') {
-        resultsMap.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-          map: resultsMap,
-          position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-  }
+  geocoder.geocode( {"address": cityInput}, function(results, status) {
+    if (status === "OK") {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
 
-  function initMap() {
-    var chicago = {lat: position.coords.latitude, lng: position.coords.longitude};
-    var indianapolis = {lat: 39.79, lng: -86.14};
 
-    var map = new google.maps.Map(document.getElementById("showMap"), {
-      center: chicago,
-      scrollwheel: false,
-      zoom: 7
-    });
-
-    document.getElementById("submit-btn").addEventListener("click", function() {
-      geocodeAddress(geocoder, map);
-    });
-
-    var directionsDisplay = new google.maps.DirectionsRenderer({
-      map: map
-    });
-
-    var request = {
-      destination: indianapolis,
-      origin: chicago,
-      travelMode: 'DRIVING'
-    };
-
-    var directionsService = new google.maps.DirectionsService();
-
-    directionsService.route(request, function(response, status) {
-      if (status == 'OK') {
-        directionsDisplay.setDirections(response);
-      }
-    });
-  }
+$(document).ready(function(event) {
+  $("#city-form").submit(function(event) {
+    event.preventDefault();
+    var cityInput = $("#city-name").val();
+    createMap(cityInput);
+  });
 });
