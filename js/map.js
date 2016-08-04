bdffe42ade739c310db691cbb412bf3be4a447fd
Alexander Jacks and Shradha Pulla <alexander+shradha@>
2016-08-04 11:21:10 -0700

    Modify geocodeAddress() method to take in an inputted city, map not working

--- js/map.js
+++ js/map.js
@@ -1,10 +1,79 @@
-// function initMap() {
-//   var mapDiv = document.getElementById("showMap");
-//   var mapCoordinates = {
-//     center: {lat: 45.5231, lng: 122.6765},
-//     zoom: 8
+
+// var addresses = [];
+//
+// $.get("http://calagator.org/events").then(function(address){
+//   address
+// });
+//
+// //User Functions
+// function geolocationSuccess(position) {
+//   var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+//
+//   var myOptions = {
+//     zoom : 16,
+//     center : userLatLng,
+//     mapTypeId : google.maps.MapTypeId.ROADMAP
 //   };
-//   var map = new google.maps.Map(mapDiv, mapCoordinates);
+//   var mapObject = new google.maps.Map(document.getElementById("showMap"), myOptions);
+//   new google.maps.Marker({
+//     map: mapObject,
+//     position: userLatLng
+//   });
 // }
 //
-// exports.mapModule = initMap;
+// function locateUser() {
+//   if (navigator.geolocation){
+//     var positionOptions = {
+//       enableHighAccuracy: true,
+//       timeout: 1000 * 1000
+//     };
+//     navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+//   }
+//   else {
+//     alert("Your browser doesn't support the Geolocation API");
+//   }
+// }
+//
+// //Geolocation Error Function
+// function geolocationError(positionError) {
+//   alert(positionError);
+// }
+
+// $(document).ready(function() {
+//   function initMap() {
+//     var chicago = {lat: 41.85, lng: -87.65};
+//     var indianapolis = {lat: 39.79, lng: -86.14};
+//
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//       center: chicago,
+//       scrollwheel: false,
+//       zoom: 7
+//     });
+//
+//     var directionsDisplay = new google.maps.DirectionsRenderer({
+//       map: map
+//     });
+//
+//     // Set destination, origin and travel mode.
+//     var request = {
+//       destination: indianapolis,
+//       origin: chicago,
+//       travelMode: 'DRIVING'
+//     };
+//
+//     // Pass the directions request to the directions service.
+//     var directionsService = new google.maps.DirectionsService();
+//     directionsService.route(request, function(response, status) {
+//       if (status == 'OK') {
+//         // Display the route on the map.
+//         directionsDisplay.setDirections(response);
+//       }
+//     });
+//   }
+// });
+
+//
+//
+// $(document).ready(function() {
+//   $("#userLocation").click(locateUser);
+// });
